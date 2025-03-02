dc54c8e2fb63b0106d3ab117c02955f9b46a84d6
Enzo Hugonnier <enzohugonn@gmail.com>
2025-03-02 21:58:04 +0100

    feat(Main): Make use of new json structure

--- main.cpp
+++ main.cpp
@@ -1,31 +1,48 @@
-#include "src/Hero.h"
+#include "src/Core/Parser.h"
+#include "src/Core/World.h"
 #include "src/UI/UISystem.h"
 #include "src/UI/Utils.h"
-
-#include <nlohmann/json.hpp>
+#include <filesystem>
 
 int main()
 {
-  nlohmann::json j;
-
-  UISystem       user_interface;
   Utils::clear_screen();
+
+  Parser                parser;
+  std::filesystem::path path("./data/Locations.json");
+  std::ifstream         file      = parser.open_file(path);
+  nlohmann::json        json      = parser.stream_to_json(file);
+  auto                  locations = parser.parse_locations(json);
+
+  Utils::clear_screen();
+
+  UISystem user_interface;
   user_interface.greet();
 
-  Hero player;
+  auto player = std::make_shared<Hero>();
 
-  std::cout << "Tell me your name please . . ." << std::endl;
+  std::cout << "Tell me your name please..." << std::endl;
   std::cout << "name >";
 
   std::string temp_name;
   std::getline(std::cin, temp_name);
-  player.set_name(temp_name);
+  player->set_name(temp_name);
+
+  World world(player, std::move(locations));
 
   user_interface.display_rules();
 
   Utils::clear_screen();
 
-  while(player.get_hp() > 0) {
-    player.display_actions();
+  while(player->get_hp() > 0) {
+    world.display_actions_for_current_location();
+    int        option_count = world.get_current_location_action_count();
+    Range      range {.begin = 1, .end = option_count};
+
+    const auto input = user_interface.prompt_user_for_index_selection(
+        "Select what to do", range
+    );
+
+    world.execute_action_at(input);
   }
 }

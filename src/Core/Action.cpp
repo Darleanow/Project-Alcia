309de74bfad7bbaab0614708b34ace8e6e59882f
Enzo Hugonnier <enzohugonn@gmail.com>
2025-03-02 21:59:33 +0100

    feat(Actions): Create class for actions with executor

---
+++ src/Core/Action.cpp
@@ -0,0 +1,43 @@
+#include "Action.h"
+#include "../Hero.h"
+#include "World.h"
+#include <any>
+
+Action::Action(
+    const std::string &action_name, const std::string &action_description,
+    std::unordered_map<std::string, std::any> action_results
+)
+    : m_name(std::move(action_name)),
+      m_description(std::move(action_description)),
+      m_results(std::move(action_results))
+{
+}
+
+Action::~Action() = default;
+
+std::string_view Action::get_name() const
+{
+  return m_name;
+}
+
+std::string_view Action::get_description() const
+{
+  return m_description;
+}
+
+void Action::execute(World &world, std::shared_ptr<Hero> player)
+{
+  for(const auto &[key, value] : m_results) {
+    if(key == "location_change") {
+      try {
+        const auto &location_name = std::any_cast<std::string>(value);
+        world.set_location(location_name);
+      } catch(const std::bad_any_cast &e) {
+        std::cout << e.what() << '\n';
+      }
+    } else if(key == "trigger_encounter") {
+      player->combat_main();
+    } else if(key == "open_menu") {
+    }
+  }
+}
---
+++ src/Core/Action.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include <any>
+#include <memory>
+#include <string>
+#include <unordered_map>
+
+class World;
+class Hero;
+
+class Action
+{
+public:
+  Action(
+      const std::string &action_name, const std::string &action_description,
+      std::unordered_map<std::string, std::any> results
+  );
+  ~Action();
+
+  [[nodiscard]] std::string_view get_name() const;
+  [[nodiscard]] std::string_view get_description() const;
+
+  void execute(World &world, std::shared_ptr<Hero> player);
+
+private:
+  std::string                               m_name;
+  std::string                               m_description;
+  std::unordered_map<std::string, std::any> m_results;
+};

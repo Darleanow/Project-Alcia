c6676bb31601870a27d716d8c20f9d8b6e13b9e8
Enzo Hugonnier <enzohugonn@gmail.com>
2025-03-05 22:49:14 +0100

    feat(Game): Enhance main game loop with player name input and improved UI interactions

--- main.cpp
+++ main.cpp
@@ -1,13 +1,16 @@
 #include "src/Core/Parser.h"
 #include "src/Core/World.h"
+#include "src/UI/SystemRelated.h"
 #include "src/UI/UISystem.h"
-#include "src/UI/Utils.h"
+#include "src/UI/Utils/DrawUtils.h"
 
 #include <filesystem>
 #include <iostream>
 
 int main()
+
 {
+
   Utils::clear_screen();
 
   Parser                parser;
@@ -16,35 +19,42 @@ int main()
   nlohmann::json        json      = parser.stream_to_json(file);
   auto                  locations = parser.parse_locations(json);
 
-  Utils::clear_screen();
-
-  UISystem user_interface;
+  UISystem              user_interface;
   user_interface.greet();
 
-  auto player = std::make_shared<Hero>();
+  std::cout << "What's your name?\n";
+  std::cout << ">";
 
-  std::cout << "Tell me your name please..." << std::endl;
-  std::cout << "name >";
+  std::string player_name;
+  std::getline(std::cin, player_name);
 
-  std::string temp_name;
-  std::getline(std::cin, temp_name);
-  player->set_name(temp_name);
-
+  auto  player = std::make_shared<Hero>(player_name);
   World world(player, std::move(locations));
 
   user_interface.display_rules();
 
-  Utils::clear_screen();
-
   while(player->get_hp() > 0) {
-    world.display_actions_for_current_location();
+
+    user_interface.display_informations_of_current_location(
+        world.get_current_location()
+    );
+    user_interface.display_actions_for_current_location(
+        world.get_current_location()
+    );
     size_t     option_count = world.get_current_location_action_count();
     Range      range {.begin = 1, .end = option_count};
 
-    const auto input = user_interface.prompt_user_for_index_selection(
-        "Select what to do", range
-    );
+    const auto input = user_interface.prompt_user_for_index_selection(range);
 
     world.execute_action_at(static_cast<size_t>(input));
   }
+
+  Utils::clear_screen();
+
+  std::cout << color(ColorType::RED) << "Game Over!"
+            << color(ColorType::DEFAULT) << std::endl;
+
+  std::cout << "Thanks for playing Project: Alcia!" << std::endl;
+
+  return 0;
 }

a684ff7ea34636d3c178208699d28bc3e7737148
Enzo Hugonnier <enzohugonn@gmail.com>
2025-03-05 22:55:34 +0100

    refactor(DrawUtils): Remove unused drawing utilities and SysCall header

--- src/UI/Utils/DrawUtils.h
+++ src/UI/Utils/DrawUtils.h
@@ -1,5 +1,4 @@
 #pragma once
-#include "System/SysCall.h"
 #include <cstdlib>
 
 namespace Utils {
@@ -7,8 +6,4 @@ static inline void clear_screen()
 {
   system("clear");
 }
-
-void   draw_box(int sx, int sy, int ex, int ey);
-
-Coords get_terminal_size();
 } // namespace Utils

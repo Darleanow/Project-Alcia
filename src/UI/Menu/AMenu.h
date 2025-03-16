a1f0c45c9c7f55d4ed86aa8d1588e78900ca6ab6
Enzo Hugonnier <enzohugonn@gmail.com>
2025-03-16 23:47:00 +0100

    feat(UI): Refactor AMenu class to improve method organization and add show_actions method

--- src/UI/Menu/AMenu.h
+++ src/UI/Menu/AMenu.h
@@ -8,9 +8,11 @@ public:
   AMenu(const std::shared_ptr<Hero> &player) : m_player(player) {}
   virtual ~AMenu() noexcept = default;
 
-  virtual void execute()        = 0;
-  virtual void draw_gui() const = 0;
+  virtual void execute() = 0;
 
 protected:
   std::shared_ptr<Hero> m_player;
+
+  virtual void          draw_gui() const     = 0;
+  virtual void          show_actions() const = 0;
 };

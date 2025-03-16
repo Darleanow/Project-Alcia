082218d57e441171ebfb186edb62eaaf31cbe897
Enzo Hugonnier <enzohugonn@gmail.com>
2025-03-16 23:45:13 +0100

    feat(Core): Add EquipmentLocation enum and utility function for mapping string values

---
+++ src/Core/CoreDefinitions.h
@@ -0,0 +1,12 @@
+#pragma once
+
+enum class EquipmentLocation
+{
+  PrimaryHand = 0,
+  Helmet      = 1,
+  Chestplate  = 2,
+  Leggings    = 3,
+  Boots       = 4,
+  Potion      = 5,
+  Loot        = 6
+};

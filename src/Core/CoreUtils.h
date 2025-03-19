f3c796e64513652eac9810a3fca340383f98d7db
Enzo Hugonnier <enzohugonn@gmail.com>
2025-03-19 23:56:47 +0100

    feat(CoreDefinitions, CoreUtils): Add Rarity, ItemType, TargetType, and EffectType enums; implement generic lookup function

--- src/Core/CoreUtils.h
+++ src/Core/CoreUtils.h
@@ -2,29 +2,68 @@
 
 #include "CoreDefinitions.h"
 
+#include <array>
 #include <optional>
 #include <string_view>
 #include <unordered_map>
 
 namespace CoreUtils {
 
-static inline std::optional<EquipmentLocation>
-    to_equipment_location(std::string_view value)
+template <typename T, size_t N>
+inline std::optional<T> lookup(
+    std::string_view                                     key,
+    const std::array<std::pair<std::string_view, T>, N> &mapping
+)
 {
-  static const std::unordered_map<std::string_view, EquipmentLocation> mapping =
-      {
-          {"Primary Hand", EquipmentLocation::PrimaryHand},
-          {"Helmet",       EquipmentLocation::Helmet     },
-          {"Chestplate",   EquipmentLocation::Chestplate },
-          {"Leggings",     EquipmentLocation::Leggings   },
-          {"Boots",        EquipmentLocation::Boots      }
-  };
+  static const std::unordered_map<std::string_view, T> map(
+      mapping.begin(), mapping.end()
+  );
 
-  if(const auto it = mapping.find(value); it != mapping.end()) {
+  if(auto it = map.find(key); it != map.end())
     return it->second;
-  }
 
   return std::nullopt;
 }
 
-}; // namespace CoreUtils
+inline std::optional<EquipmentLocation>
+    to_equipment_location(std::string_view value)
+{
+  static constexpr std::array<std::pair<std::string_view, EquipmentLocation>, 5>
+      mapping = {
+          {{"primary_hand", EquipmentLocation::PrimaryHand},
+           {"helmet", EquipmentLocation::Helmet},
+           {"chestplate", EquipmentLocation::Chestplate},
+           {"leggings", EquipmentLocation::Leggings},
+           {"boots", EquipmentLocation::Boots}}
+  };
+
+  return lookup(value, mapping);
+}
+
+inline std::optional<Rarity> string_to_rarity(std::string_view rarity_string)
+{
+  static constexpr std::array<std::pair<std::string_view, Rarity>, 4> mapping =
+      {
+          {{"common", Rarity::Common},
+           {"rare", Rarity::Rare},
+           {"epic", Rarity::Epic},
+           {"legendary", Rarity::Legendary}}
+  };
+
+  return lookup(rarity_string, mapping);
+}
+
+inline std::optional<ItemType> string_to_type(std::string_view type_string)
+{
+  static constexpr std::array<std::pair<std::string_view, ItemType>, 4>
+      mapping = {
+          {{"loot", ItemType::Loot},
+           {"potion", ItemType::Potion},
+           {"weapon", ItemType::Weapon},
+           {"armor", ItemType::Armor}}
+  };
+
+  return lookup(type_string, mapping);
+}
+
+} // namespace CoreUtils

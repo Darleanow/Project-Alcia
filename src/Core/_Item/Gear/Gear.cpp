5d08ed14e517d328689262fab036e3ac3f86cad9
Enzo Hugonnier <enzohugonn@gmail.com>
2025-03-21 23:52:59 +0100

    feat(Gear): Enhance Gear class with rarity attribute and stat range methods

--- src/Core/_Item/Gear/Gear.cpp
+++ src/Core/_Item/Gear/Gear.cpp
@@ -2,10 +2,22 @@
 
 Gear::Gear(
     ItemRecipe recipe, EquipmentLocation equipment_location, std::string name,
-    std::string description, int price, Stats stats
+    std::string description, Rarity rarity, int price, Stats low_range_stats,
+    Stats high_range_stats
 )
     : CrafteableMixin<Gear>(std::move(recipe)),
       EquipeableMixin<Gear>(std::move(equipment_location)),
-      Item(std::move(name), std::move(description), price)
+
+      Item(std::move(name), std::move(description), rarity, price)
 {
+}
+
+Stats Gear::get_low_stats_range() const
+{
+  return m_low_range_stats;
+}
+
+Stats Gear::get_high_stats_range() const
+{
+  return m_high_range_stats;
 }
--- src/Core/_Item/Gear/Gear.h
+++ src/Core/_Item/Gear/Gear.h
@@ -12,9 +12,15 @@ class Gear final : public CrafteableMixin<Gear>,
 public:
   Gear(
       ItemRecipe recipe, EquipmentLocation equipment_location, std::string name,
-      std::string description, int price, Stats stats
+      std::string description, Rarity rarity, int price, Stats low_range_stats,
+      Stats high_range_stats
   );
   ~Gear() noexcept override = default;
 
+  Stats get_low_stats_range() const;
+  Stats get_high_stats_range() const;
+
 private:
+  Stats m_low_range_stats;
+  Stats m_high_range_stats;
 };

a2dd4e4c0de328e57f811cd16509494af63f5930
Enzo Hugonnier <enzohugonn@gmail.com>
2025-03-22 23:06:52 +0100

    refactor(God): Simplify monster generation and remove unused item generation functions

--- src/Legacy/god.h
+++ src/Legacy/god.h
@@ -1,12 +1,5 @@
 #pragma once
 
-#include "Item.h"
 #include "Monster.h"
 
-#include <memory>
-
-Monster              *generate_monster(Hero *hero);
-
-std::unique_ptr<Item> generate_item();
-
-Item *random_item(std::vector<Item *> items, std::vector<size_t> odds);
+Monster *generate_monster(Hero *hero);

79e17c502344ec1c8923191e8d58d159b245f617
Enzo Hugonnier <enzohugonn@gmail.com>
2025-03-22 23:07:28 +0100

    feat(ItemRegistry): Implement random item generation and rarity weighting

--- src/Core/ItemRegistry/ItemRegistry.h
+++ src/Core/ItemRegistry/ItemRegistry.h
@@ -1,9 +1,11 @@
 #pragma once
 
+#include "Item/Gear/Gear.h"
 #include <memory>
 #include <unordered_map>
 
 class Item;
+class GearInstance;
 
 class ItemRegistry
 {
@@ -14,10 +16,16 @@ public:
     return registry;
   }
 
-  void                  register_item(std::shared_ptr<Item> item);
+  void                          register_item(std::shared_ptr<Item> item);
 
-  std::shared_ptr<Item> get_item(const std::string &name);
+  std::shared_ptr<Item>         get_item(const std::string &name) const;
+
+  std::unique_ptr<GearInstance> generate_random_item(int player_level) const;
 
 private:
   std::unordered_map<std::string, std::shared_ptr<Item>> m_items;
+
+  std::unique_ptr<GearInstance>     generate_item(const Gear &gear) const;
+
+  std::unordered_map<Rarity, float> get_rarity_weights(int player_level) const;
 };
